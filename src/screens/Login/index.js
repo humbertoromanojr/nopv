003f5ae1fecd1d55d3be386a942051c52f27e392
JR Dev <astronomi@gmail.com>
2020-04-05 01:20:32 -0300

    update files

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../../styles/styles.css'
+import Header from '../../components/NavBar';
+
+import elevation from '../../assets/images/elevation.png'
 
 export default function Login(props) {
 
   return (
     <div className="main-content">
       
+      <Header />
       <div className="container">
-        Login screen
+     
+        <div className="container-text">
+          <label className="title">Login</label>
+        </div>
+        
       </div>
 
-      <div><Link className="back" to="/">Back</Link></div>
+      <div className="container-elevation">
+        <img src={elevation} className="image-elevation" alt="image elevation" />
+      </div>
       
     </div>
   );
